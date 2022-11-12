fa34e914143ec092196a699dbdd8fcb0c0dc98ab
Mervin Soriano <95460290+mervinsoriano@users.noreply.github.com>
2022-11-12 11:48:13 -0800

    Updated Style and Script
    * Yay! Video is working

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Get Video
+const video = document.querySelector("video");
+
+// Navigator object
+if (navigator.mediaDevices.getUserMedia) {
+  
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true
+    })
+    .then((stream) => (video.srcObject = stream))
+    .catch((error) => console.log(error));
+}
